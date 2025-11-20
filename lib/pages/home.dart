33fa4a42ffc43e15ae91f1d15398fb85966eed6f
monic22-us <monsemorales367@gmail.com>
2025-11-19 23:32:52 -0600

    Creating a Home Screen Page UI from Scratch_p1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,38 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xffe3e6ff), Color(0xfff1f3ff), Colors.white],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.location_on_outlined),
+                Text(
+                  'jiombang, East jave',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+//me quede en el minuto 15
